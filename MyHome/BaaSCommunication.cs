4140b093f3ffb11ea14c766783e6f3b1732cb03e
Veikko Krypczyk <veikko2000@yahoo.de>
2020-12-19 22:38:13 +0100

    Add Class BaaSCommunication Add Parse.Net2StandardLibrary

---
+++ MyHome/BaaSCommunication.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHome
+{
+    public class BaaSCommunication
+    {
+        // Individual Keys
+        //const string applicationId = "ZaP3heCxmOgl5ulgGosDyuG6SgHbf8b8EUfWOf0i";
+        //const string serverURL = "https://parseapi.back4app.com/";
+        //const string netKey = "Wz3paUpU3i2S6xBbkk3w9eAc5EchAx2NSY5MREuS";
+
+        public BaaSCommunication()
+        {
+            // Parse Initlization
+            //ParseClient.Initialize(new ParseClient.Configuration
+            //{
+            //    ApplicationId = applicationId,
+            //    WindowsKey = netKey,
+            //    Server = serverURL
+            //});
+
+        }
+
+
+        //public async void DeleteAllMyMessagesAsync()
+        //{
+        //    var query = ParseObject.GetQuery("Message").WhereEqualTo("UserName", Settings.UserName);
+        //    IEnumerable<ParseObject> results = await query.FindAsync();
+        //    foreach (var item in results)
+        //    {
+        //        //    await item.DeleteAsync();
+        //    }
+        //}
+
+        
+    }
+}
