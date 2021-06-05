72ff7131ce00132ff389ae2210e602e0c8b4a504
Veikko Krypczyk <veikko2000@yahoo.de>
2021-06-05 23:36:53 +0200

    Exit Button in Pane

--- MyHome/ViewModels/MainPageViewModel.cs
+++ MyHome/ViewModels/MainPageViewModel.cs
@@ -1,11 +1,23 @@
 ﻿using MyHome.Helpers;
 using MyHome.Settings;
+using Windows.ApplicationModel.Core;
 using Windows.UI.Xaml;
 
 namespace MyHome.ViewModels
 {
     public class MainPageViewModel : Observable
     {
+        public MainPageViewModel()
+        {
+            ShutDownCommand = new RelayCommand(OnShutDownCommandExecute);
+        }
+
+        private void OnShutDownCommandExecute()
+        {
+            CoreApplication.Exit();
+        }
+
+        public RelayCommand ShutDownCommand { get; set; }
         public bool AutoChangeContentSections
         {
             get
